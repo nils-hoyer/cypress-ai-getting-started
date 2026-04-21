5e003e87966cb29ec76cb4c3371d11206dc43665
nils.hoyer <hoyer.nils@gmail.com>
2026-04-21 15:16:03 +0200

    aktualisiere cypress.config.js: entferne specPattern aus der e2e Konfiguration

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,6 @@ export default defineConfig({
   e2e: {
     projectId: 'tjnqsr',
     baseUrl: 'https://test-boutique.vercel.app',
-    specPattern: 'cypress/e2e/**/*.cy.{ts,tsx,js,jsx}',
   },
   defaultCommandTimeout: 5000,
 })
